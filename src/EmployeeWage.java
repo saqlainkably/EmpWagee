bafaaea89f297afca240bc2e60e8149647399d39
Your Name <saqlainzakaria05@gmail.com>
2024-07-18 14:54:59 +0530

    Added: EmpPart/Full Time Updated

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,6 @@
 public class EmployeeWage {
     public static void main(String[] args) {
+        int IS_PART_TIME = 1;
         int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
 
@@ -7,7 +8,10 @@ public class EmployeeWage {
         int empWage  = 0;
 
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME){
+        if (empCheck == IS_PART_TIME) {
+            empHours = 4;
+        }
+        else if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is Present");
             empHours = 8;
         }
