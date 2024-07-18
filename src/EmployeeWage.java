b204541a6e7899808458b200e1f17595b4dca42f
Your Name <saqlainzakaria05@gmail.com>
2024-07-18 15:20:26 +0530

    Added: Employee Hours Calculated

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,23 +1,25 @@
 public class EmployeeWage {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
 
         int empHours = 0;
         int empWage  = 0;
 
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PART_TIME) {
-            empHours = 4;
-        }
-        else if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present");
-            empHours = 8;
-        }
-        else{
-            System.out.println("Employee is Absent");
-            empHours = 0;
+
+        switch (empWage){
+            case IS_PART_TIME:
+                empHours = 4;
+                break;
+            case IS_FULL_TIME:
+                empHours = 8;
+                break;
+            default:
+                empHours = 0;
         }
         empWage = empHours * EMP_RATE_PER_HOUR;
         System.out.println("EmpWage: " + empWage);
