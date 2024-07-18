12914c779badb2e984d390bcfba1dd43d16b8683
Your Name <saqlainzakaria05@gmail.com>
2024-07-18 14:36:29 +0530

    Added: UC1

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,6 +1,13 @@
 public class EmployeeWage {
     public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
     }
+
 }
